2cfa2159b2c2762aa32bfb623140d9d2c708c369
SonnyCoco <129166978+SonnyCoco@users.noreply.github.com>
2025-11-25 15:54:31 +0200

    sales etl some version

--- shell/run_sales.sh
+++ shell/run_sales.sh
@@ -3,15 +3,15 @@
 
 # Check for argument
 if [ -z "$1" ]; then
-  echo " Usage: ./run_sales_job.sh <YYYY-MM-DD>"
+  echo " Usage: ./run_sales.sh <YYYY-MM-DD>"
   exit 1
 fi
 
 RUN_DATE="$1"
 
-echo "Triggering sales_etl_job.py for $RUN_DATE..."
+echo "Triggering sales_etl.py for $RUN_DATE..."
 
 # Run spark-submit inside spark-master container
-docker exec spark-master spark-submit /opt/spark-apps/scripts/sales_etl_job.py "$RUN_DATE"
+docker exec spark-master spark-submit /opt/spark-apps/sales_etl/scripts/sales_etl_job.py "$RUN_DATE"
 
 echo "Job completed for $RUN_DATE"
