f06f88918a9a9dbcf23036b01b68fa0e00985b10
Sonny Croco <129166978+sonnycroco@users.noreply.github.com>
2025-11-29 12:40:49 +0200

    Update run_sales.sh
    Add usage instructions and argument check for date input.

--- shell/run_sales.sh
+++ shell/run_sales.sh
@@ -1,6 +1,13 @@
 
 #!/bin/bash
 
+
+# This script requires one argument: a date in the format YYYY-MM-DD.
+# If no argument is provided, it prints a usage message and exits.
+# The provided date is stored in RUN_DATE and passed to a Spark job.
+# The script then executes spark-submit inside the spark-master
+# Docker container to run the sales_etl_job.py Spark application with the given date.
+
 # Check for argument
 if [ -z "$1" ]; then
   echo " Usage: ./run_sales.sh <YYYY-MM-DD>"
